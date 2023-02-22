cd307095a425dd974e28ffb22f9d57ed056c7772
`thais` <`tjlannes@gmail.com`>
2023-02-23 00:52:02 +0100

    change get method and add post method to create new items in the list

--- src/api/things-i-know/things-i-know-router.ts
+++ src/api/things-i-know/things-i-know-router.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getThingsController } from './things-i-know-controllers.js';
+import {
+  getThingsController,
+  createThingsController,
+} from './things-i-know-controllers.js';
 
 const router = express.Router();
 
-router.route('/').get(getThingsController);
+router.route('/').get(getThingsController).post(createThingsController);
 
 export default router;
